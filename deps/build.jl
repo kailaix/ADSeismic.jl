8ea464e09af7a70d8713f7f8fe1c72d20fd6dc79
Kailai Xu <kailaix@stanford.edu>
2020-04-23 15:53:17 -0700

    update build file

--- deps/build.jl
+++ deps/build.jl
@@ -1,15 +1,8 @@
 using ADCME
 
-function install_binary(DIR)
-    cd(DIR)
-    rm("build", recursive=true, force=true)
-    mkdir("build")
-    cd("build")
-    ADCME.cmake(DIR)
-    ADCME.make()
-end
-
-install_binary("$(@__DIR__)/CustomOps/AcousticWaveSource")
-install_binary("$(@__DIR__)/CustomOps/SourceOps")
-install_binary("$(@__DIR__)/CustomOps/ReceiveOps")
-
+cd("CustomOps")
+rm("build", force=true, recursive=true)
+mkdir("build")
+cd("build")
+ADCME.cmake()
+ADCME.make()

2e07f22ddf738ad3425478ac79e472658d4bc05e
Kailai Xu <kailaix@hotmail.com>
2020-09-08 11:48:36 -0700

    update

--- examples/mpi_acoustic/weak_scaling/MPI_backward.sh
+++ examples/mpi_acoustic/weak_scaling/MPI_backward.sh
@@ -1,5 +1,5 @@
 for n in 1 4 9 16 25 36 49 64 81 100 
 do 
-    salloc -n $n -c 8 --mem=10240 mpirun -n $n julia MPI_backward.jl &
+    salloc -n $n --ntasks-per-node=1 mpirun -n $n julia MPI_backward.jl &
 done 
 wait

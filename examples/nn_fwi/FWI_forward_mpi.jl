10c62558a38484063e8bf5bee40d51f7c9c48014
weiqiang <wayne.weiqiang@gmail.com>
2021-06-13 22:59:10 -0700

    tried to add dependency between sources. But the rurnning is super slow

--- examples/nn_fwi/FWI_forward_mpi.jl
+++ examples/nn_fwi/FWI_forward_mpi.jl
@@ -13,8 +13,8 @@ ADCME.options.customop.verbose = false
 
 py"""
 import tensorflow as tf
-config_proto = tf.ConfigProto(intra_op_parallelism_threads=1,
-                              inter_op_parallelism_threads=1)
+config_proto = tf.ConfigProto(intra_op_parallelism_threads=0,
+                              inter_op_parallelism_threads=0)
 # from tensorflow.core.protobuf import rewriter_config_pb2
 # off = rewriter_config_pb2.RewriterConfig.OFF
 # config_proto.graph_options.rewrite_options.memory_optimization  = off
@@ -36,7 +36,7 @@ mpi_init()
 model_name = "models/marmousi2-model-true.mat"
 # model_name = "models/BP-model-true.mat"
 
-param = load_params(model_name, "MPIElastic", vp_ref=3e3)
+param = load_params(model_name, "MPIElastic")
 compute_PML_Params!(param)
 
 vp = matread(model_name)["vp"]
@@ -49,20 +49,42 @@ rho = extract_local_patch(param, rho)
 
 λ, μ, ρ = compute_lame_parameters(vp, vs, rho)
 
-src = load_elastic_source(model_name, use_mpi=true, param=param)[1:1]
-receiver = load_elastic_receiver(model_name, use_mpi=true, param=param)[1:1]
+# src = load_elastic_source(model_name, use_mpi=true, param=param)[4:4]
+# receiver = load_elastic_receiver(model_name, use_mpi=true, param=param)[4:4]
+# propagator = x->MPIElasticPropagatorSolver(param, x, ρ, λ, μ)
+# [MPISimulatedObservation!(propagator(src[i]), receiver[i]) for i = 1:length(src)]
+# Rs_ = [receiver[i].rcvv for i = 1:length(receiver)]
 
-propagator = x->MPIElasticPropagatorSolver(param, x, ρ, λ, μ)
-[MPISimulatedObservation!(propagator(src[i]), receiver[i]) for i = 1:length(src)]
-Rs_ = [receiver[i].rcvv for i = 1:length(receiver)]
+src = load_elastic_source(model_name, use_mpi=true, param=param)
+receiver = load_elastic_receiver(model_name, use_mpi=true, param=param)
+
+Rs_ = []
+# for i = 1:length(src)
+for i = 4:4
+  src_ = src[i]
+  receiver_ = receiver[i]
+
+  propagator = MPIElasticPropagatorSolver(param, src_, ρ, λ, μ)
+  MPISimulatedObservation!(propagator, receiver_)
+  
+  if (i < length(src)) && !ismissing(src[i+1].srcv) 
+    src[i+1].srcv += sum(propagator.vx[end,:]) * 1e-20
+  end
+
+  push!(Rs_, receiver_.rcvv)
+end
 
 sess = Session(config=config_proto); init(sess)
 
 @info "starting..."
-Rs = run(sess, Rs_)
 
+@time Rs = run(sess, Rs_)
 @info "saving..."
-@save "data/dat$(mpi_rank())-$(mpi_size()).jld2" Rs
+@save "data/data$(mpi_rank())-$(mpi_size()).jld2" Rs
+
+# vx = run(sess, propagator(src[1]).vx)
+# @info "saving..."
+# @save "data/vx$(mpi_rank())-$(mpi_size()).jld2" vx
 
 if mpi_rank()==0 && mpi_size()==1
   ii = 1 # source number
--- examples/nn_fwi/FWI_inversion_mpi.jl
+++ examples/nn_fwi/FWI_inversion_mpi.jl
@@ -15,10 +15,10 @@ ADCME.options.customop.verbose = false
 py"""
 import tensorflow as tf
 from tensorflow.core.protobuf import rewriter_config_pb2
-config_proto = tf.ConfigProto(intra_op_parallelism_threads=1,
-                              inter_op_parallelism_threads=1)
-off = rewriter_config_pb2.RewriterConfig.OFF
-config_proto.graph_options.rewrite_options.memory_optimization  = off
+config_proto = tf.ConfigProto(intra_op_parallelism_threads=0,
+                              inter_op_parallelism_threads=0)
+# off = rewriter_config_pb2.RewriterConfig.OFF
+# config_proto.graph_options.rewrite_options.memory_optimization  = off
 """
 config_proto = py"config_proto"
 @show config_proto
@@ -37,12 +37,18 @@ mpi_init()
 # model_name = "models/BP-model-true.mat"
 model_name = "models/marmousi2-model-smooth.mat"
 
-param = load_params(model_name, "MPIElastic", vp_ref=3e3)
+param = load_params(model_name, "MPIElastic")
 compute_PML_Params!(param)
 
 vp = matread(model_name)["vp"]
 vs = matread(model_name)["vs"]
 rho = matread(model_name)["rho"]
+##
+vs = matread("models/marmousi2-model-true.mat")["vs"]
+rho = matread("models/marmousi2-model-true.mat")["rho"]
+
+@info "loading data..."
+@load "data/data$(mpi_rank())-$(mpi_size()).jld2" Rs
 
 vp_ = Variable(vp)
 var_size = size(vp_)
@@ -57,27 +63,42 @@ rho = extract_local_patch(param, rho)
 
 λ, μ, ρ = compute_lame_parameters(vp, vs, rho)
 
-src = load_elastic_source(model_name, use_mpi=true, param=param)[1:1]
-receiver = load_elastic_receiver(model_name, use_mpi=true, param=param)[1:1]
+# src = load_elastic_source(model_name, use_mpi=true, param=param)[4:4]
+# receiver = load_elastic_receiver(model_name, use_mpi=true, param=param)[4:4]
+# propagator = x->MPIElasticPropagatorSolver(param, x, ρ, λ, μ)
+# [MPISimulatedObservation!(propagator(src[i]), receiver[i]) for i = 1:length(src)]
 
-propagator = x->MPIElasticPropagatorSolver(param, x, ρ, λ, μ)
-[MPISimulatedObservation!(propagator(src[i]), receiver[i]) for i = 1:length(src)]
+src = load_elastic_source(model_name, use_mpi=true, param=param)
+receiver = load_elastic_receiver(model_name, use_mpi=true, param=param)
 
-@info "restoring..."
-@load "data/dat$(mpi_rank())-$(mpi_size()).jld2" Rs
+rcvv = Rs[1]
+for i = 4:4
 
-Rs_ = [receiver[i].rcvv for i = 1:length(receiver)]
+  src_ = src[i]
+  receiver_ = receiver[i]
 
-local_loss = sum(sum((Rs - Rs_).^2))
+  propagator = MPIElasticPropagatorSolver(param, src_, ρ, λ, μ)
+  MPISimulatedObservation!(propagator, receiver_)
 
-loss = mpi_sum(local_loss)
+  local_loss = sum(propagator.vx[end,:]) * 1e-20
+  if !isnothing(rcvv)
+      global local_loss += sum((receiver_.rcvv - rcvv )^2) 
+  end
 
+end
+
+loss = mpi_sum(local_loss) * 1e7
 g = gradients(loss, vp_)
 
 sess = Session(config=config_proto); init(sess)
 
+@info "running loss"
+run(sess, loss)
+@info "running gradients"
+run(sess, g)
+
 @info "starting..."
-@info "Initial loss = ", run(sess, loss)
+@info "initial local loss = $(run(sess, local_loss)) at rank = $(mpi_rank())"
 
 function calculate_loss(x)
     x = reshape(x, var_size)
@@ -85,11 +106,19 @@ function calculate_loss(x)
     L
 end
 
+iter = 0
+iter_result = Array{Float64, 2}[]
 function calculate_gradients(G, x)
     x = reshape(x, var_size)
     G[:] = run(sess, g, vp_=>x)
-end
 
+    global iter += 1
+    if iter % 100 == 1
+      clf()
+      pcolormesh(x', cmap="jet")
+      savefig("test_figures/x-$(iter).png")
+    end
+end
 
 losses = Float64[]
 function step_callback(x)
